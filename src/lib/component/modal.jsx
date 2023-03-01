a7881a13af3658bbff600219b289aa167ed45def
Willem Jouret <willem.jouret44@gmail.com>
2023-03-01 20:05:15 +0100

    remove test

--- src/lib/component/modal.jsx
+++ src/lib/component/modal.jsx
@@ -1,27 +1,17 @@
-import React, { forwardRef } from "react"
-import { useRef } from "react"
+import React from "react"
 import "./modal.css"
 
 function Modal({ isOpen, onClose, title, subTitle, content }) {
-  const ref = useRef(null)
-  useEffect(() => {
-    if (ref.current) {
-      ref.current.focus()
-    }
-  }, [])
-  const Button = forwardRef((props, ref) => (
-<button ref={ref}>{props.children}</button>
-  ))
   if (isOpen) {
     return (
-      <dialog className='modal'>
+      <dialog aria-modal="true" className='modal'>
         <section className='section'>
           <header className='header'>
             <h1 className='title'>{title}</h1>
             <span className='subtitle'>{subTitle}</span>
-            <Button ref={ref} className='btn' onClick={onClose}>
+            <button  className='btn' onClick={onClose}>
               x
-            </Button>
+            </button>
           </header>
           <div className='content-container'>{content}</div>
           <footer className='footer'></footer>
